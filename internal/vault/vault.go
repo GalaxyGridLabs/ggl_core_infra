080d08cc7aa7d9afc543f1f7389dc5a8dd0ce0dc
Hulto <7121375+hulto@users.noreply.github.com>
2024-08-01 23:58:21 +0000

    Just hacking

--- internal/vault/vault.go
+++ internal/vault/vault.go
@@ -1,50 +1,66 @@
 package vault
 
 import (
+	"fmt"
+
 	"github.com/pulumi/pulumi-gcp/sdk/v7/go/gcp/cloudrun"
 	"github.com/pulumi/pulumi-gcp/sdk/v7/go/gcp/kms"
 	"github.com/pulumi/pulumi-gcp/sdk/v7/go/gcp/projects"
 	"github.com/pulumi/pulumi-gcp/sdk/v7/go/gcp/serviceaccount"
 	"github.com/pulumi/pulumi-gcp/sdk/v7/go/gcp/storage"
+	"github.com/pulumi/pulumi-random/sdk/v4/go/random"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-func NewVault(ctx *pulumi.Context, gcpProject string, gcpRegion string) error {
-	// Create key for vault
-	vaultKeyRingName := "vault-keys-xise3a"
+type HashicorpVault struct {
+	pulumi.ResourceState
+	Id  string
+	Url pulumi.StringPtrOutput
+}
 
-	// vaultKeyRing, err := kms.GetKMSKeyRing(ctx, &kms.GetKMSKeyRingArgs{
-	// 	Name:     vaultKeyRingName,
-	// 	Location: "us-east4",
-	// }, nil)
-	// if err != nil {
-	// 	return err
-	// }
+func NewVault(ctx *pulumi.Context, gcpProject string, gcpRegion string, resourceId string) (*HashicorpVault, error) {
 
-	vaultKeyRing, err := kms.NewKeyRing(ctx, "keyring", &kms.KeyRingArgs{
-		Name:     pulumi.String(vaultKeyRingName),
-		Location: pulumi.String("global"),
-	})
+	vaultRes := &HashicorpVault{
+		Id: resourceId,
+	}
+	err := ctx.RegisterComponentResource(fmt.Sprintf("pkg:index:gcp:HashicorpVault:%s", resourceId), "vault", vaultRes)
 	if err != nil {
-		return err
+		return nil, err
+	}
+
+	// Create key for vault
+	gcpKmsRegion := "global"
+	vaultKeyRing, err := kms.NewKeyRing(ctx, "vault-keys", &kms.KeyRingArgs{
+		Location: pulumi.String(gcpKmsRegion),
+	}, pulumi.Parent(vaultRes))
+	if err != nil {
+		return nil, err
 	}
 
 	unsealKey, err := kms.NewCryptoKey(ctx, "vault-key", &kms.CryptoKeyArgs{
-		Name:           pulumi.String("vault-key-ci8kie"),
 		KeyRing:        vaultKeyRing.ID(),
 		RotationPeriod: pulumi.String("100000s"),
-	})
+	}, pulumi.Parent(vaultRes))
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	// Create service account for vault
-	vaultSvcAccount, err := serviceaccount.NewAccount(ctx, "vault-svc", &serviceaccount.AccountArgs{
-		AccountId:   pulumi.String("svc-vault-storage"),
-		DisplayName: pulumi.String("Vault Storage Admin"),
-	})
+	vaultSvcAccNameHex, err := random.NewRandomId(ctx, "vault-svc-id", &random.RandomIdArgs{
+		ByteLength: pulumi.Int(6),
+	}, pulumi.Parent(vaultRes))
 	if err != nil {
-		return err
+		return nil, err
+	}
+
+	vaultSvcAccount, err := serviceaccount.NewAccount(ctx, "vault-svc", &serviceaccount.AccountArgs{
+		AccountId: vaultSvcAccNameHex.Hex.ApplyT(func(h string) (string, error) {
+			return fmt.Sprintf("svc-vault-storage-%s", h), nil
+		}).(pulumi.StringOutput),
+		DisplayName: pulumi.String("Vault Storage Admin"),
+	}, pulumi.Parent(vaultRes))
+	if err != nil {
+		return nil, err
 	}
 
 	// Assign service account permissions
@@ -56,36 +72,36 @@ func NewVault(ctx *pulumi.Context, gcpProject string, gcpRegion string) error {
 		Project: pulumi.String(gcpProject),
 		Role:    pulumi.String("roles/storage.objectAdmin"),
 		Member:  saEmail,
-	})
+	}, pulumi.Parent(vaultRes))
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	_, err = kms.NewKeyRingIAMMember(ctx, "vaultSvcKeyRingRole", &kms.KeyRingIAMMemberArgs{
+	_, err = kms.NewKeyRingIAMMember(ctx, "vault-svc-key-ring-role", &kms.KeyRingIAMMemberArgs{
 		KeyRingId: vaultKeyRing.ID(),
 		Member:    saEmail,
 		Role:      pulumi.String("roles/owner"),
-	})
+	}, pulumi.Parent(vaultRes))
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	// Create bucket to store vault stuff
-	vaultStorage, err := storage.NewBucket(ctx, "vault_storage", &storage.BucketArgs{
-		Name:                   pulumi.String("ggl_vault_storage_aec0hiarugeigeing"),
+	vaultStorage, err := storage.NewBucket(ctx, "ggl-vault-storage", &storage.BucketArgs{
 		Location:               pulumi.String("US"),
 		ForceDestroy:           pulumi.Bool(true),
 		PublicAccessPrevention: pulumi.String("enforced"),
-	})
+	}, pulumi.Parent(vaultRes))
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	// vault config
-	configValue := pulumi.All(vaultStorage.Name, unsealKey.Name).ApplyT(
+	configValue := pulumi.All(vaultStorage.Name, unsealKey.Name, vaultKeyRing.Name).ApplyT(
 		func(args []interface{}) pulumi.StringOutput {
 			bucketName := args[0]
 			unsealkeyName := args[1]
+			vaultKeyRingName := args[2]
 			return pulumi.Sprintf(`
 
 			ui              = true
@@ -104,7 +120,7 @@ func NewVault(ctx *pulumi.Context, gcpProject string, gcpRegion string) error {
 					region      = "%s"
 					key_ring    = "%s"
 					crypto_key  = "%s"
-				}`, bucketName, gcpProject, "global", vaultKeyRingName, unsealkeyName)
+				}`, bucketName, gcpProject, gcpKmsRegion, vaultKeyRingName, unsealkeyName)
 		},
 	).(pulumi.StringInput)
 
@@ -112,9 +128,10 @@ func NewVault(ctx *pulumi.Context, gcpProject string, gcpRegion string) error {
 		Name:  pulumi.String("VAULT_LOCAL_CONFIG"),
 		Value: configValue,
 	}
+	// 						Image: pulumi.String("docker.io/hashicorp/vault:1.17.2@sha256:aaaedf0b3b34560157cc7c06f50f794eb7baa071165f2eed4db94b44db901806"),
 
 	// Create a Cloud Run service definition.
-	service, err := cloudrun.NewService(ctx, "new-vault", &cloudrun.ServiceArgs{
+	service, err := cloudrun.NewService(ctx, "vault", &cloudrun.ServiceArgs{
 		Location: pulumi.String("us-central1"),
 		Template: cloudrun.ServiceTemplateArgs{
 			Spec: cloudrun.ServiceTemplateSpecArgs{
@@ -147,25 +164,30 @@ func NewVault(ctx *pulumi.Context, gcpProject string, gcpRegion string) error {
 				},
 			},
 		},
-	})
+	}, pulumi.Parent(vaultRes))
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	// Make vault publicly accessible
-	_, err = cloudrun.NewIamBinding(ctx, "vaultSvcPublicBinding", &cloudrun.IamBindingArgs{
+	_, err = cloudrun.NewIamBinding(ctx, "vault-svc-public-binding", &cloudrun.IamBindingArgs{
 		Location: service.Location,
 		Service:  service.Name,
 		Role:     pulumi.String("roles/run.invoker"),
 		Members: pulumi.StringArray{
 			pulumi.String("allUsers"),
 		},
-	})
+	}, pulumi.Parent(vaultRes))
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	ctx.Export("url", service.Statuses.Index(pulumi.Int(0)).Url())
+	ctx.RegisterResourceOutputs(vaultRes, pulumi.Map{
+		"url": service.Statuses.Index(pulumi.Int(0)).Url(),
+		"id":  pulumi.String(resourceId),
+	})
 
-	return nil
+	vaultRes.Url = service.Statuses.Index(pulumi.Int(0)).Url()
+
+	return vaultRes, nil
 }
--- main.go
+++ main.go
@@ -3,17 +3,61 @@ package main
 import (
 	"ggl_core_infra/internal/vault"
 
+	"github.com/pulumi/pulumi-random/sdk/v4/go/random"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
+const randIdLength = 6
+
+func randomId(ctx *pulumi.Context, name string) (pulumi.StringOutput, error) {
+	// Create service account for vault
+	vaultSvcAccNameHex, err := random.NewRandomId(ctx, name, &random.RandomIdArgs{
+		ByteLength: pulumi.Int(randIdLength),
+	})
+	if err != nil {
+		return pulumi.StringOutput{}, err
+	}
+
+	return vaultSvcAccNameHex.Hex, nil
+}
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
+
 		project := "galaxygridlabs"
 		region := "us-central1"
-		err := vault.NewVault(ctx, project, region)
+
+		r1, err := randomId(ctx, "test1")
 		if err != nil {
 			return err
 		}
+		r1.ApplyT(func(id string) (*vault.HashicorpVault, error) {
+			vault, err := vault.NewVault(ctx, project, region, id)
+			if err != nil {
+				return nil, err
+			}
+			return vault, nil
+		})
+
+		r2, err := randomId(ctx, "test2")
+		if err != nil {
+			return err
+		}
+		r2.ApplyT(func(id string) (*vault.HashicorpVault, error) {
+			vault, err := vault.NewVault(ctx, project, region, id)
+			if err != nil {
+				return nil, err
+			}
+			return vault, nil
+		})
+
+		// vault, err := vault.NewVault(ctx, project, region, "test")
+		// if err != nil {
+		// 	return err
+		// }
+
+		// ctx.Export("url", vault.Url)
+
 		return nil
 	})
 }
