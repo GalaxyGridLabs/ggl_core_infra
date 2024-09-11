b1b350de30f34af9668f01f5888a734a82274e54
Hulto <7121375+hulto@users.noreply.github.com>
2024-09-11 23:57:07 +0000

    Saving

--- main.go
+++ main.go
@@ -28,59 +28,74 @@ func randomId(ctx *pulumi.Context, name string) (pulumi.StringOutput, error) {
 	return vaultSvcAccNameHex.Hex, nil
 }
 
+func setupVault(ctx *pulumi.Context, project string, region string, labZone string) (*vault.HashicorpVault, error) {
+	vaultMain, err := vault.NewVault(ctx, project, region, "main")
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: add DNS mapping
+	cloudRunDns := vaultMain.MapOutput["url"].(pulumi.StringInput).ToStringOutput().ApplyT(func(u string) string {
+		parsed, _ := url.Parse(u)
+		var res_host string
+		if !strings.HasSuffix(parsed.Host, ".") {
+			res_host = fmt.Sprintf("%s.", parsed.Host)
+		} else {
+			res_host = parsed.Host
+		}
+		return res_host
+	}).(pulumi.StringInput)
+
+	vaultCname, err := common.NewDnsRecord(ctx, "vault-dns", "vault2", labZone, "CNAME", cloudRunDns)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx.Export("private_vault_url", vaultMain.MapOutput["url"])
+	ctx.Export("public_vault_domain", pulumi.String(vaultCname.DnsName))
+
+	return vaultMain, nil
+}
+
+func setupGitea(ctx *pulumi.Context, project string, region string, labZone string) (*git.Gitea, error) {
+	gitMain, err := git.NewGitea(ctx, project, region, "main")
+	if err != nil {
+		return nil, err
+	}
+
+	tmp_arr := gitMain.MapOutput["url"].(compute.InstanceNetworkInterfaceArrayOutput)
+	git_nat_ip := tmp_arr.Index(pulumi.Int(0)).AccessConfigs().Index(pulumi.Int(0)).NatIp()
+
+	tmp := git_nat_ip.ApplyT(func(n *string) string {
+		return *n
+	}).(pulumi.StringInput)
+
+	gitDns, err := common.NewDnsRecord(ctx, "gitea-dns", "git", labZone, "A", tmp)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx.Export("git_url", pulumi.String(gitDns.DnsName))
+
+	return gitMain, nil
+}
+
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 
 		project := "galaxygridlabs"
 		region := "us-central1"
+		labZone := "galaxygridlabs-com"
 
 		// _, err := common.NewNetwork(ctx, "lab-net", project, region, "main")
 		// if err != nil {
 		// 	return err
 		// }
 
-		vaultMain, err := vault.NewVault(ctx, project, region, "main")
+		vaultMain, err := setupVault(ctx, project, region, labZone)
 		if err != nil {
 			return err
 		}
-
-		cloudRunDns := vaultMain.MapOutput["url"].(pulumi.StringInput).ToStringOutput().ApplyT(func(u string) string {
-			parsed, _ := url.Parse(u)
-			var res_host string
-			if !strings.HasSuffix(parsed.Host, ".") {
-				res_host = fmt.Sprintf("%s.", parsed.Host)
-			} else {
-				res_host = parsed.Host
-			}
-			return res_host
-		}).(pulumi.StringInput)
-
-		vaultCname, err := common.NewDnsRecord(ctx, "vault-dns", "vault2", "galaxygridlabs-com", "CNAME", cloudRunDns)
-		if err != nil {
-			return err
-		}
-
-		ctx.Export("private_vault_url", vaultMain.MapOutput["url"])
-		ctx.Export("public_vault_domain", pulumi.String(vaultCname.DnsName))
-
-		gitMain, err := git.NewGitea(ctx, project, region, "main")
-		if err != nil {
-			return err
-		}
-
-		tmp_arr := gitMain.MapOutput["url"].(compute.InstanceNetworkInterfaceArrayOutput)
-		git_nat_ip := tmp_arr.Index(pulumi.Int(0)).AccessConfigs().Index(pulumi.Int(0)).NatIp()
-
-		tmp := git_nat_ip.ApplyT(func(n *string) string {
-			return *n
-		}).(pulumi.StringInput)
-
-		gitDns, err := common.NewDnsRecord(ctx, "gitea-dns", "git", "galaxygridlabs-com", "A", tmp)
-		if err != nil {
-			return err
-		}
-
-		ctx.Export("git_url", pulumi.String(gitDns.DnsName))
 
 		// Stage 2
 		vaultMain.MapOutput.ToMapOutput().ApplyT(func(t map[string]interface{}) error {
@@ -96,6 +111,13 @@ pulumi config set vault:token %s --secret
 			// if err != nil {
 			// 	return err
 			// }
+
+			// Setup OIDC with google
+
+			_, err = setupGitea(ctx, project, region, labZone)
+			if err != nil {
+				return err
+			}
 
 			return nil
 		})
