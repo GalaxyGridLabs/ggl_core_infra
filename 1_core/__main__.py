12b2eda4e269e21c977bda158d708d27bf39de4d
Hulto <7121375+hulto@users.noreply.github.com>
2024-11-03 20:44:46 +0000

    Formatting

--- 1_core/__main__.py
+++ 1_core/__main__.py
@@ -1,3 +1,21 @@
 """A Python Pulumi program"""
 
 import pulumi
+
+
+def setupVault():
+    print("test")
+
+
+def main():
+    print("Starting")
+
+
+"""
+
+    """
+# Setup vault
+
+
+if __name__ == "__main__":
+    main()
