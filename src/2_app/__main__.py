ccc2baf82b310cb87f9498be3eee5fada54f96c5
Hulto <7121375+hulto@users.noreply.github.com>
2024-11-09 03:17:27 +0000

    gitea auto register user and admin status

--- src/2_app/__main__.py
+++ src/2_app/__main__.py
@@ -1,5 +1,6 @@
 """A Python Pulumi program"""
 
+import json
 import os
 import pulumi
 import pulumi_vault as pvault
@@ -21,17 +22,47 @@ def main():
                 name=name,
                 policy=f.read())
 
+    config = pulumi.Config("ggl")
+    sa_secret = config.require("vault_sa_account_json")
+    sa_email = json.loads(sa_secret)["client_email"]
+    gsuite_admin = config.require("gsuite_admin")
+    gsuite_domain = config.require("gsuite_domain")
+
     # Setup vault google auth method
     google_auth = AuthMethodJWT(
         name="google-auth",
         path="oidc",
         desc="Authenticate to lab using GSuite account",
-        discover_url="https://accounts.google.com")
+        discover_url="https://accounts.google.com",
+        oidc_scopes=["openid","email","profile"],
+        provider_config={
+            "provider": "gsuite",
+            "gsuite_service_account": sa_secret,
+            "gsuite_admin_impersonate": gsuite_admin,
+            "fetch_groups": True,
+            "domain": gsuite_domain,
+            "fetch_user_info": True,
+            "groups_recurse_max_depth": 5,
+            "impersonate_principal": sa_email,
+        },
+        claim_mappings={
+            "email": "email",
+            "groups": "groups",
+            "given_name": "nickname",
+        })
 
     # Setup vault oidc provider for gitea
+    def gen_accessor_template(accessor: str):
+        return f"""{{ 
+    "username":{{{{identity.entity.aliases.{accessor}.name}}}},
+    "groups": {{{{identity.entity.groups.names}}}},
+    "email": {{{{identity.entity.aliases.{accessor}.metadata.email}}}},
+    "nickname": {{{{identity.entity.aliases.{accessor}.metadata.nickname}}}}
+}}"""
     gitea_oidc = OIDCProvider(
         name="gitea-auth",
-        redirect_uris=["http://localhost:3000/user/oauth2/vault/callback"])
+        redirect_uris=["http://localhost:3000/user/oauth2/vault/callback", "https://openidconnect.net/callback"],
+        scope_template=google_auth.auth_accessor.apply(lambda accessor: gen_accessor_template(accessor) ))
 
     pulumi.export("client_id", gitea_oidc.client_id)
     pulumi.export("client_secret", gitea_oidc.client_secret)
