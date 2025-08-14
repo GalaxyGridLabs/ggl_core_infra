91bb3b04ec1b9889d02718663f169f01e26ae29e
hulto <7121375+hulto@users.noreply.github.com>
2025-08-14 04:05:09 +0000

    format

--- src/shared/vault/pki.py
+++ src/shared/vault/pki.py
@@ -3,22 +3,22 @@ import re
 import pulumi_vault as vault
 from ..constants import YEARS
 
-MAX_TTL = 2*YEARS
+MAX_TTL = 2 * YEARS
+
 
 class PKI(pulumi.ComponentResource):
-    def __init__(self,
-                name: str,
-                opts = None):
+    def __init__(self, name: str, opts=None):
 
-        super().__init__('ggl:shared/vault:PKI', name, None, opts)
+        super().__init__("ggl:shared/vault:PKI", name, None, opts)
 
         mount = vault.mount.Mount(
             resource_name=name,
             path=name,
             type="pki",
             max_lease_ttl_seconds=MAX_TTL,
-            opts=pulumi.ResourceOptions(parent=self))
-        
+            opts=pulumi.ResourceOptions(parent=self),
+        )
+
         self.mount = mount
 
         root_cert = vault.pkisecret.SecretBackendRootCert(
@@ -31,8 +31,9 @@ class PKI(pulumi.ComponentResource):
             ou="Galaxy Grid",
             organization="Labs",
             exclude_cn_from_sans=True,
-            opts=pulumi.ResourceOptions(parent=self))
-        
+            opts=pulumi.ResourceOptions(parent=self),
+        )
+
         vault_config = pulumi.Config("vault")
         self.vault_address = vault_config.require("address")
 
@@ -41,18 +42,16 @@ class PKI(pulumi.ComponentResource):
             backend=mount.path,
             crl_distribution_points=[f"{self.vault_address}/v1/{name}/ca"],
             issuing_certificates=[f"{self.vault_address}/v1/{name}/crl"],
-            opts=pulumi.ResourceOptions(parent=self))
-    
-    def create_cert(
-            self,
-            name: str,
-            domain: str):
-        
+            opts=pulumi.ResourceOptions(parent=self),
+        )
+
+    def create_cert(self, name: str, domain: str) -> tuple[str, str]:
+
         # Extract the domain without subdomains
         m = re.search(r"([a-zA-Z0-9-]+)(\.[a-zA-Z]{2,5})?(\.[a-zA-Z]+$)", domain)
         assert m is not None, f"could not extract domain from {domain}"
         lab_domain = "".join([g for g in m.groups() if g is not None])
-        
+
         role = vault.pkisecret.SecretBackendRole(
             resource_name=name,
             name=name,
@@ -60,14 +59,16 @@ class PKI(pulumi.ComponentResource):
             ttl=MAX_TTL,
             allowed_domains=[lab_domain],
             allow_subdomains=True,
-            opts=pulumi.ResourceOptions(parent=self))
+            opts=pulumi.ResourceOptions(parent=self),
+        )
 
         certificate = vault.pkisecret.SecretBackendCert(
             resource_name=name,
             name=role.name,
             common_name=domain,
             backend=self.mount.path,
-            ttl=1*YEARS,
-            opts=pulumi.ResourceOptions(parent=self))
+            ttl=1 * YEARS,
+            opts=pulumi.ResourceOptions(parent=self),
+        )
 
         return certificate.certificate, certificate.private_key
