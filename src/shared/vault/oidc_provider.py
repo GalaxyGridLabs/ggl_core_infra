13dd6d75b4eaac0cdbb061f6f70c4309734319f1
hulto <7121375+hulto@users.noreply.github.com>
2025-09-21 02:51:10 +0000

    Format

--- src/shared/vault/oidc_provider.py
+++ src/shared/vault/oidc_provider.py
@@ -6,14 +6,17 @@ import re
 import json
 from shared.constants import DAYS
 
-class OIDCProvider(pulumi.ComponentResource):
-    def __init__(self,
-                name: str,
-                redirect_uris: List[str],
-                scope_template: pulumi.Output,
-                opts = None):
 
-        super().__init__('ggl:shared/vault:OIDCProvider', name, None, opts)
+class OIDCProvider(pulumi.ComponentResource):
+    def __init__(
+        self,
+        name: str,
+        redirect_uris: List[str],
+        scope_template: pulumi.Output,
+        opts=None,
+    ):
+
+        super().__init__("ggl:shared/vault:OIDCProvider", name, None, opts)
 
         vault_config = pulumi.Config("vault")
         self.vault_address = vault_config.require("address")
@@ -21,9 +24,10 @@ class OIDCProvider(pulumi.ComponentResource):
         key = pvault.identity.OidcKey(
             resource_name=name,
             allowed_client_ids=["*"],
-            rotation_period=1*DAYS,
-            verification_ttl=1*DAYS,
-            opts=pulumi.ResourceOptions(parent=self))
+            rotation_period=1 * DAYS,
+            verification_ttl=1 * DAYS,
+            opts=pulumi.ResourceOptions(parent=self),
+        )
 
         client = pvault.identity.OidcClient(
             resource_name=name,
@@ -33,15 +37,17 @@ class OIDCProvider(pulumi.ComponentResource):
             assignments=["allow_all"],
             id_token_ttl=2400,
             access_token_ttl=7200,
-            opts=pulumi.ResourceOptions(parent=self))
+            opts=pulumi.ResourceOptions(parent=self),
+        )
 
         scope = pvault.identity.OidcScope(
             resource_name=name,
             name=name,
             template=scope_template,
             description=f"Scope for the {name} oidc provider",
-            opts=pulumi.ResourceOptions(parent=self))
-        
+            opts=pulumi.ResourceOptions(parent=self),
+        )
+
         issuer_host = self.vault_address.split("://")[1]
         provider = pvault.identity.OidcProvider(
             resource_name=name,
@@ -50,20 +56,25 @@ class OIDCProvider(pulumi.ComponentResource):
             issuer_host=issuer_host,
             allowed_client_ids=[client.client_id],
             scopes_supporteds=[scope.name],
-            opts=pulumi.ResourceOptions(parent=self))
-        
+            opts=pulumi.ResourceOptions(parent=self),
+        )
+
         self.client_id = client.client_id
         self.client_secret = client.client_secret
-        self.issuer_base = f"{self.vault_address}/v1/identity/oidc/provider/{provider.name}"
+        self.issuer_base = (
+            f"{self.vault_address}/v1/identity/oidc/provider/{provider.name}"
+        )
 
         return
 
     @property
     def vault_address(self):
         return self.__vault_address
-    
+
     @vault_address.setter
     def vault_address(self, value: str):
         regex = r"https:\/\/[a-z0-9-.]+"
-        assert re.fullmatch(regex, value) is not None, f"vault_address {value} must match {regex}"
+        assert (
+            re.fullmatch(regex, value) is not None
+        ), f"vault_address {value} must match {regex}"
         self.__vault_address = value
